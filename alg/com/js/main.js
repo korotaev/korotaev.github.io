471c13109811fef8e6c93bd4f43479fd944b1957
Oleg Korotaev <korotaev@me.com>
2019-02-28 18:28:22 +0300

    fasdf

--- alg/com/js/main.js
+++ alg/com/js/main.js
@@ -1,7 +1,59 @@
-var forest = {
+// <div class="picture pic-01">
+//     <div class="bubble bub_time-01 bub-01_01"></div>
+//     <div class="bubble bub_time-02 bub-01_02"></div>
+//     <div class="bubble bub_time-03 bub-01_03"></div>
+//     </div>
+//
 
+var pics = [];
 
 function init() {
+
+    $('.picture').each(function (e, t) {
+        var classList = $(t).attr('class').split(/\s+/);
+        $.each(classList, function(index, item) {
+            if (item.match('pic-')) {
+                pics.push([Math.round($(t).position().top),item]);
+                var num = item.replace('pic-','');
+                $(t).css({'backgroundImage': 'url(./img/'+num+'.png)'});
+            }
+        });
+    });
+
+    $('.bubble').each(function (e, t) {
+        var classList = $(t).attr('class').split(/\s+/);
+        $.each(classList, function(index, item) {
+            if (item.match('bub-')) {
+                var num = item.replace('bub-','');
+                var b = $('<div/>').addClass('bubble_context');
+                var img = $('<img/>').attr('src', './img/bubbles/'+num+'.png');
+                b.append(img);
+                $(t).append(b);
+            }
+        });
+    });
+
+    window.addEventListener('scroll', function(e) {
+        var s = window.scrollY;
+        for(var i in pics) {
+            if(pics[i][0] < s + 300) {
+                $('.'+pics[i][1]).find('.bubble').each(function (e,t) {
+                    var classList = $(t).attr('class').split(/\s+/);
+                    $.each(classList, function(index, item) {
+                        if (item.match('bub_time-')) {
+                            var tim = item.replace('bub_time-','');
+                            $(t).addClass('bubble_time-'+tim);
+                        }
+                    });
+                });
+                pics.splice(i, 1);
+            }
+        }
+
+    });
+
+
+
 
 }
 
