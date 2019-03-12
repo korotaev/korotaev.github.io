77be11a4de8e42099d28eb5751dacd784f0c14e3
Oleg Korotaev <korotaev@me.com>
2019-03-12 19:40:35 +0300

    fasdf

---
+++ alg/congrats/js/main.js
@@ -0,0 +1,24 @@
+    /*
+
+function init() {
+
+    for (var i=0; i<90; i++) {
+
+        var a = Math.PI*2*Math.random();
+        var l = rand(100,265);
+        var x = Math.round(Math.cos(a)*l), y = Math.round(Math.sin(a)*l);
+
+        var str = ".spark-"+(i+1)+" { left: "+x+"px; top: "+y+"px; }";
+        // console.log(str);
+        //console.log("<div class=\"spark spark-"+(i+1)+" spark-color-"+(i%3+1)+"\"></div>");
+    }
+
+
+
+}
+
+function rand(min, max) {
+    return Math.floor(Math.random()*(max-min+1)) + min;
+}
+
+        */
