e4b0b319f2984cbc9800652c6a1113fd6657ce8d
Oleg Korotaev <oleg.korotaev@gmail.com>
2020-11-12 13:46:12 +0300

    fsdff

--- car/img/newcar/back/_go.ps1
+++
@@ -1,1 +0,0 @@
-magick montage back*.png -resize 200x120 -background none -tile 6x12 -geometry +0+0 ..\back.png
--- car/img/newcar/diffuse/_go.ps1
+++
@@ -1,1 +0,0 @@
-magick montage diffuse*.png -resize 200x120 -background none -tile 6x12 -geometry +0+0 ..\diffuse.png
