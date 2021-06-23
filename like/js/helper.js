ba1ba8530882f1e566a9be527f57132bd275c6e8
Oleg Korotaev <oleg.korotaev@gmail.com>
2021-06-23 12:07:27 +0300

    fasdf

---
+++ like/js/helper.js
@@ -0,0 +1,119 @@
+/**
+ * Created by korotaev on 11/01/2018.
+ */
+
+function screenShot(obj) {
+    html2canvas(obj,{
+        // useCORS: false, proxy: false,
+        allowTaint: false,
+        useCORS: true,
+        logging: true
+    }).then(
+
+        function (canvas) {
+            var link = document.createElement("a");
+            link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+            link.download = filename+'.png';
+            link.click();
+        });
+}
+
+function easeInOutCubic(t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t*t + b;
+    t -= 2;
+    return c/2*(t*t*t + 2) + b;
+}
+
+function bezier3(p, t) {
+    var x = (1-t)*(1-t)*(1-t)*p[0][0] + 3*t*(1-t)*(1-t)*p[1][0] + 3*t*t*(1-t)*p[2][0] + t*t*t*p[3][0];
+    var y = (1-t)*(1-t)*(1-t)*p[0][1] + 3*t*(1-t)*(1-t)*p[1][1] + 3*t*t*(1-t)*p[2][1] + t*t*t*p[3][1];
+    return [x,y];
+}
+function bezier2(p0, p1, p2, t) {
+    var x = (1-t)*(1-t)*p0.x + 2*t*(1-t)*p1.x + t*t*p2.x,
+        y = (1-t)*(1-t)*p0.y + 2*t*(1-t)*p1.y + t*t*p2.y;
+    return {x:x, y:y};
+}
+function roughBezierLength(p0, p1, p2) {
+    var s = 0, total = 20;
+    for(var i=0; i<total; i++) {
+        s += distance(
+            bezier2(p0,p1,p2, i/total),
+            bezier2(p0,p1,p2, (i+1)/total)
+        );
+    }
+    return s;
+}
+function getLine(p0,p1) {
+    var a = p0.y - p1.y;
+    var b = p1.x - p0.x;
+    var c = p0.x*p1.y - p1.x*p0.y;
+    return {a:a,b:b,c:c};
+}
+function middleLine(p0,p1) {
+    var mid = {x:(p0.x + p1.x)/2,y:(p0.y + p1.y)/2};
+    var line = getLine(p0,p1);
+    var midPlus = {
+        x:mid.x+line.a,
+        y:mid.y+line.b
+    };
+    return getLine(mid,midPlus);
+}
+function solveLines(l1,l2) {
+    if(l2.a*l1.b == l2.b*l1.a) {
+        return -1;
+    }
+    var y = (l2.c*l1.a - l2.a*l1.c) / (l2.a*l1.b - l2.b*l1.a),
+        x = (l2.c*l1.b - l1.c*l2.b) / (l2.b*l1.a - l1.b*l2.a);
+    return {x:x,y:y};
+}
+function distance(p0,p1) {
+    return Math.sqrt((p0.x-p1.x)*(p0.x-p1.x) + (p0.y-p1.y)*(p0.y-p1.y));
+}
+function circleAndLine(p0,p1,r) {
+    var d = distance(p0,p1);
+    var cos = (p1.x - p0.x)/d,
+        sin = (p1.y - p0.y)/d;
+    var x1 = p0.x + r*cos,
+        y1 = p0.y + r*sin;
+    var x2 = p0.x - r*cos,
+        y2 = p0.y - r*sin;
+    return [
+        {x:x2, y:y2},
+        {x:x1, y:y1}
+    ];
+}
+
+
+
+function rand(min,max) {
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+(function(console){
+
+    console.save = function(data, filename){
+
+        if(!data) {
+            console.error('Console.save: No data')
+            return;
+        }
+
+        if(!filename) filename = 'console.json'
+
+        if(typeof data === "object"){
+            data = JSON.stringify(data, undefined, 4)
+        }
+
+        var blob = new Blob([data], {type: 'text/json'}),
+            e    = document.createEvent('MouseEvents'),
+            a    = document.createElement('a')
+
+        a.download = filename
+        a.href = window.URL.createObjectURL(blob)
+        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.dispatchEvent(e)
+    }
+})(console)
---
+++ like/js/main.js
@@ -0,0 +1,138 @@
+var gui;
+
+var level = {
+    velocity: 4,
+    gravity: 2,
+    total: 10,
+    numHearts : 10,
+    scaleHeart : 0.8,
+    opacityStart : 0.7,
+    timeEnd : 0.5,
+    timeStart : 0,
+    timeLife : 1
+};
+
+var hearts = [];
+
+let start;
+var timeCur;
+
+
+function init() {
+
+    gui = new dat.GUI();
+
+    gui.add(level, 'velocity', 0.1, 8).onChange(function(value) {
+    });
+    gui.add(level, 'gravity', 0.0, 8).onChange(function(value) {
+    });
+    gui.add(level, 'numHearts', 1, 30).onChange(function(value) {
+    });
+    gui.add(level, 'scaleHeart', 0.1, 3).onChange(function(value) {
+    });
+    gui.add(level, 'opacityStart', 0.0, 1).onChange(function(value) {
+    });
+    gui.add(level, 'timeEnd', 0, 1).onChange(function(value) {
+    });
+    // gui.add(level, 'timeStart', 0, 1).onChange(function(value) {
+    // });
+    gui.add(level, 'timeLife', 0.1, 3).onChange(function(value) {
+    });
+
+
+    $('#map').click(function(e) {
+        var posX = $(this).position().left,
+        posY = $(this).position().top;
+
+        for(var i=0; i<level.numHearts; i++) {
+            var hc = $('<div/>').addClass('heart-container').appendTo('#map');
+            var h = $('<div/>').addClass('heart').appendTo(hc);
+            hc.css({'top' : e.pageY - posY, 'left' : e.pageX - posX});
+
+            var rotLength = rand(10,15);
+            var rotDir = rand(0,1);
+            var rotPos = rand(0,15-rotLength);
+            var rotStart = rotDir ? rotPos : rotPos + rotLength;
+            var rotEnd = rotDir ? rotPos + rotLength : rotPos;
+
+            var heart = {
+                'hc' : hc,
+                'h' : h,
+                'angle' : Math.PI*Math.random(),
+                'rotStart' : rotStart,
+                'rotEnd' : rotEnd,
+                'x' : e.pageX - posX,
+                'y' : e.pageY - posY,
+                'timeStart' : timeCur + level.timeLife*1000*i/level.numHearts,
+                'timeLife' : level.timeLife*1000,
+                'vel' : level.velocity,
+                'dir' : 0,
+                'opacityStart' : level.opacityStart
+            };
+            heart.dir = Math.PI/2 - heart.angle;
+            hearts.push(heart);
+        }
+
+
+
+
+
+    });
+
+    window.requestAnimationFrame(step);
+}
+
+function getSize(t) {
+    var mark1 = 0.1, mark2 = level.opacityStart, scale = level.scaleHeart;
+    if(t < mark1) {
+        return t*scale;
+    } else if(t> mark2) {
+        return (1-(t-mark2)/(1-mark2))*scale;
+    } else {
+        return scale;
+    }
+}
+
+
+function step(timestamp) {
+    if (start === undefined)
+    start = timestamp;
+    timeCur = timestamp - start;
+
+    if(hearts.length) {
+        for(var i=0; i<hearts.length; i++) {
+            var heart = hearts[i];
+            var t = (timeCur - heart.timeStart)/heart.timeLife;
+            if(t>1 || t<0) {
+                heart.hc.hide();
+                continue; //лень удалять элемент массива
+            } else {
+                heart.hc.show();
+            }
+            var rot = Math.round(heart.rotStart + t*(heart.rotEnd - heart.rotStart));
+            var backPosX = -(rot%4)*80;
+            var backPosY = -(Math.floor(rot/4))*80;
+            var opacity = t > heart.opacityStart ? 1-(t-heart.opacityStart)/(1-heart.opacityStart) : 1;
+            heart.h.css({
+                'backgroundPosition' : backPosX+'px '+backPosY+'px',
+                'opacity' : opacity
+            });
+            var deg = Math.round(heart.dir*180/Math.PI);
+            heart.hc.css({'transform' : 'rotate('+deg+'deg) scale('+getSize(t)+')'});
+
+
+            var y = heart.y-(1-t)*heart.vel*Math.sin(heart.angle)-level.gravity;
+            var x = heart.x+(1-t)*heart.vel*Math.cos(heart.angle);
+            heart.hc.css({'top' : y, 'left' : x});
+            heart.dir = Math.atan2(y-heart.y,x-heart.x)+Math.PI/2;
+            heart.y = y;
+            heart.x = x;
+        }
+    }
+
+
+
+
+
+    window.requestAnimationFrame(step);
+}
